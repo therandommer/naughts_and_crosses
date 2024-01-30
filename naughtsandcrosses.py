d41875378728f603fce518204ccd4554bf8acd28
Isabella <bdc190799@gmail.com>
2024-01-30 12:21:19 +0000

    Added more specifics and logic for next turn start
    Added the logic to start the next turn, also added functionality for an updated game grid that does not replace the original. Added logic to check if input is numeric or not. Added logic to flip the turn when necessary.

--- naughtsandcrosses.py
+++ naughtsandcrosses.py
@@ -6,7 +6,8 @@
 # O naughts
 # gameGrid and gameGridText are for the basic game board
 gameGrid = ["-" "-" "-" "\n" "-" "-" "-" "\n" "-" "-" "-" "\n"]
-gameGridText = ",".join(gameGrid)
+currentGameGrid = []  # will be updated during game loop
+gameGridText = ",".join(currentGameGrid)
 xScore = 0
 oScore = 0
 draws = 0
@@ -15,9 +16,51 @@ currentTurnText = "O"
 
 isGameOver = False  # run game logic while false, ask for rematch/reset when true
 
-introText = "Welcome to naughts and crosses. \nIt is", currentTurnText, "to go."
+introText = "Welcome to naughts and crosses. \n"
+currentTurnText = "It is", currentTurnText, "to go."
+instructionText = "Please enter a number between 1-9"
 
 
+# Initialise game state
 def startGame():
     print(introText)
+    currentGameGrid = gameGrid
     print(gameGridText)
+    print(currentTurnText)
+    print(instructionText)
+
+
+# Handles the basic logic for getting input to correlate to each square
+def getInput(input):
+    if input.isnumeric():
+        print("Input is numeric, do logic")
+    elif input.isnumeric() and input == "0":
+        print("Input is 0, return false")
+    elif input.isnumeric() and input > 9:
+        print("Input is too large, return false")
+    else:
+        print("Enter a valid number between 1-9, return false")
+
+
+# will be called after every valid input, just before the end of game logic if the game is not over
+def nextTurn():
+    if currentTurn == 1:
+        currentTurn == 2
+        currentTurnText == "X"
+    elif currentTurn == 2:
+        currentTurn == 1
+        currentTurnText == "O"
+
+
+# game loop starts here
+startGame()
+
+while not isGameOver:
+    currentInput = input()
+    getInput(currentInput)
+
+    if isGameOver:
+        print("End of game", currentTurnText, "wins!")
+        break  # exits game loop
+    else:
+        nextTurn()
