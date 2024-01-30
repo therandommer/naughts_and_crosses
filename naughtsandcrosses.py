59f0a38d00020d75d974254f4fbc2817ce213138
Isabella <bdc190799@gmail.com>
2024-01-30 14:26:57 +0000

    Turns work, the board updates correctly
    Added logic to make the turns correctly flip between players. Also added the ability for the board to reflect the new game states. Finally, updated the replacement logic to work off numbers instead of the previous method. Condensed other logic

--- naughtsandcrosses.py
+++ naughtsandcrosses.py
@@ -1,73 +1,85 @@
 # variables
 
 # possible values include:
-# - neutral
+# numbers = neutral
 # X crosses
 # O naughts
 # gameGrid and gameGridText are for the basic game board
-gameGrid = "-" "-" "-" "-" "-" "-" "-" "-" "-"
+gameGrid = "1" "2" "3" "4" "5" "6" "7" "8" "9"
 currentGameGrid = gameGrid  # will be updated during game loop
 xScore = 0
 oScore = 0
 draws = 0
 currentTurn = 1
-currentTurnText = "O"
-
+currentTurnVal = "O"
 # game logic loopers
 hasInput = False  # used to loop input getter unti valid input received
 isGameOver = False  # run game logic while false, ask for rematch/reset when true
 
 introText = "Welcome to naughts and crosses. \n"
-currentTurnText = "It is", currentTurnText, "to go."
 instructionText = "Please enter a number between 1-9"
 
 
 # Initialise game state
 def startGame():
     print(introText)
-    print(printGameGrid())
-    print(currentTurnText)
+    printGameGrid()
+    print("It is " + currentTurnVal + " to go.")
 
 
 def printGameGrid():
     gameGridText = "".join(str(currentGameGrid))
-    return "\n".join(gameGridText[i : i + 3] for i in range(0, len(gameGridText), 3))
+    return print(  # adds line separators for user view
+        "\n".join(gameGridText[i : i + 3] for i in range(0, len(gameGridText), 3))
+    )
 
 
 # Handles the basic logic for getting input to correlate to each square
 def getInput(input):
     if input.isnumeric() and int(input) > 0 and int(input) < 10:
-        print("Input is numeric, do logic")
         # add input to check if a square is occupied or not
         return True
     else:
-        print("Input is not valid, return false")
         return False
+
+
+def updateGrid(indexNo):
+    global currentGameGrid
+    newGameGrid = str(currentGameGrid)
+    newGameGrid = newGameGrid.replace(str(indexNo), str(currentTurnVal))
+    currentGameGrid = newGameGrid
+    print("Testing \n ", currentGameGrid)
+    printGameGrid()
 
 
 # will be called after every valid input, just before the end of game logic if the game is not over
 def nextTurn():
+    global currentTurn
+    global currentTurnVal
     if currentTurn == 1:
-        currentTurn == 2
-        currentTurnText == "X"
+        currentTurn = 2
+        currentTurnVal = "X"
     elif currentTurn == 2:
-        currentTurn == 1
-        currentTurnText == "O"
+        currentTurn = 1
+        currentTurnVal = "O"
+    print("It is " + currentTurnVal + " to go.")
+    global hasInput
+    hasInput = False
 
 
 # game loop starts here
 startGame()
 
 while not isGameOver:
-    print(instructionText)
-    currentInput = input()
-
     if not hasInput:
+        print(instructionText)
+        currentInput = input()
         hasInput = getInput(currentInput)
+        updateGrid(currentInput)
+        print(hasInput)
     else:
-        print("Continue game logic")
         if isGameOver:
-            print("End of game", currentTurnText, "wins!")
+            print("End of game" + currentTurnVal + "wins!")
             break  # exits game loop
         else:
             nextTurn()
